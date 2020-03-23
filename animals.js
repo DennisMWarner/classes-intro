552e8cbfaf175a977adec26bfa187d87c858d508
DennisMWarner <warner.dennis.m@gmail.com>
2020-03-23 16:38:54 -0600

    init

---
+++ animals.js
@@ -0,0 +1,499 @@
+console.log("'animals.js' linked...");
+
+class Aardvark {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Bear {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Cat {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Dog {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Elephant {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Frog {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Giraffe {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Hog {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Iguana {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Jackal {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Kangaroo {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Llama {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Mongoose {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Newt {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Octopus {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Panda {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Quail {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Raccoon {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Snake {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Tiger {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Unicorn {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Vulture {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Walrus {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Xerus {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Yak {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+class Zebra {
+  constructor(
+    name = "",
+    legs = 0,
+    tail = false,
+    size = "",
+    eats = "",
+    color = ""
+  ) {
+    this.name = name;
+    this.legs = legs;
+    this.tail = tail;
+    this.size = size;
+    this.eats = eats;
+    this.color = color;
+  }
+  feedIt(food) {}
+  petIt() {}
+}
+let bear = new Bear("Smokey", 4, true, "large", "everything");
+let polarBear = new Bear("Snowflake", 4, true, "large", "seals", "white");
+console.log(bear, polarBear);
